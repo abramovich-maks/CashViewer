4ceb7b13a82e130ca293ae659228485078d8a3dc
abramovich-maks <maks.abramovich.1995@mail.ru>
2026-05-23 20:08:58 +0200

    Initial commit

---
+++ src/main/java/com/cashviewer/CashViewerApplication.java
@@ -0,0 +1,13 @@
+package com.cashviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cashviewer/CashViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
